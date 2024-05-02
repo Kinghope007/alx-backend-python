9dfcefce282800b05099cbdf22a2acb36b231d72
Kinghope007 <emmanuelhope36@gmail.com>
2024-05-02 21:03:07 +0000

    advanced annnotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from typing import Sequence, Any, Union
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Returns the first element of a sequence or None if the sequence is empty.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping, key: Any, default: T = None) -> Union[Any, T]:
+    """
+    Safely gets a value from a dictionary based on the given key.
+    Returns the value associated with the key if it exists, otherwise returns the default value.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
